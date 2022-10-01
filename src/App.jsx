d6c1ad915614fc68bdeec10e8667eaedc28ee375
tom <xtom2301@gmail.com>
2022-10-01 22:49:32 +0200

    navbar, main-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { MainSection } from './components';
+
 const App = () => {
-  return <div className='App'>Hello World</div>;
+  return (
+    <div>
+      <main className='px-6'>
+        <section className='min-h-screen'>
+          <MainSection />
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,42 @@
+import { BsMoonStarsFill } from 'react-icons/bs';
+import {
+  AiFillTwitterCircle,
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from 'react-icons/ai';
+
+const MainSection = () => {
+  return (
+    <div>
+      <nav className='flex justify-between items-center mt-4 mb-10'>
+        <h1 className='text-xl font-bold'>devtom.me</h1>
+        <div className='flex items-center space-x-8'>
+          <BsMoonStarsFill className='cursor-pointer text-2xl' />
+          <button className='bg-gradient-to-r from-indigo-500 to-purple-500 text-white px-4 py-2 rounded-md'>
+            Portfolio
+          </button>
+        </div>
+      </nav>
+      <div className='text-center p-6'>
+        <h1 className='text-4xl py-2 font-bold text-indigo-500'>Tom Martin</h1>
+        <h3 className='text-2xl py-2'>Developer and learner.</h3>
+        <p className='py-5 text-gray-600'>
+          I create Websites, Apps and little Projects for myself or friends.
+          Join me down below to get and stay in touch!
+        </p>
+      </div>
+      <div className='text-4xl flex justify-center gap-4 py-2 text-gray-600'>
+        <AiFillGithub />
+        <AiFillLinkedin />
+        <AiFillTwitterCircle />
+        <AiFillInstagram />
+      </div>
+      <div className='overflow-hidden mx-auto bg-gradient-to-b from-indigo-500 to-purple-500 rounded-full w-80 h-62 mt-20'>
+        <img src='me.png' alt='me' className='object-cover' />
+      </div>
+    </div>
+  );
+};
+
+export default MainSection;

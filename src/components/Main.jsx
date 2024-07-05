bc792928785b2005a1cc0fffe463c14c3ae73c7f
tmartin <tmartin231@outlook.de>
2024-07-05 17:44:19 +0200

    new pfp

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,28 +3,28 @@ import {
   AiFillGithub,
   AiFillInstagram,
   AiFillLinkedin,
-} from 'react-icons/ai';
+} from "react-icons/ai";
 const Main = () => {
   return (
-    <div className='sm:flex sm:justify-center sm:flex-col min-h-screen'>
-      <div className='text-center p-6'>
-        <h1 className='text-4xl py-2 font-bold text-indigo-500 '>Tom Martin</h1>
-        <h3 className='text-2xl py-2 dark:text-white'>
+    <div className="sm:flex sm:justify-center sm:flex-col min-h-screen">
+      <div className="text-center p-6">
+        <h1 className="text-4xl py-2 font-bold text-indigo-500 ">Tom Martin</h1>
+        <h3 className="text-2xl py-2 dark:text-white">
           Junior-Developer and learner.
         </h3>
-        <p className='py-5 text-gray-600 sm:w-96 sm:mx-auto dark:text-gray-400'>
-          I create Websites, Apps and little Projects for myself or friends.
+        <div className="overflow-hidden mx-auto rounded-3xl w-72 h-72 m-6 shadow-lg shadow-gray-700 dark:shadow-gray-400">
+          <img src="me.JPG" alt="me" className="object-cover" />
+        </div>
+        <p className="py-5 text-gray-600 sm:w-96 sm:mx-auto dark:text-gray-400">
+          I create Websites and little Projects for myself, friends or clients.
           Join me down below to get and stay in touch!
         </p>
       </div>
-      <div className='text-4xl flex justify-center gap-4 py-2 text-gray-600'>
-        <AiFillGithub className='cursor-pointer dark:text-white' />
-        <AiFillLinkedin className='cursor-pointer dark:text-white' />
-        <AiFillTwitterCircle className='cursor-pointer dark:text-white' />
-        <AiFillInstagram className='cursor-pointer dark:text-white' />
-      </div>
-      <div className='overflow-hidden mx-auto bg-gradient-to-b from-indigo-500 to-purple-500 rounded-full w-80 h-62 mt-10'>
-        <img src='me.png' alt='me' className='object-cover' />
+      <div className="text-4xl flex justify-center gap-4 py-2 text-gray-600">
+        <AiFillGithub className="cursor-pointer dark:text-white" />
+        <AiFillLinkedin className="cursor-pointer dark:text-white" />
+        <AiFillTwitterCircle className="cursor-pointer dark:text-white" />
+        <AiFillInstagram className="cursor-pointer dark:text-white" />
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import { BsMoonStarsFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { BsMoonStarsFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ setDarkMode, darkMode }) => {
   return (
-    <div className='bg-white dark:bg-gray-800 sticky top-0 z-10'>
-      <nav className='max-w-screen-lg mx-auto py-6 px-0 flex justify-between items-center mb-10 sm:px-4'>
-        <Link to='/' className='text-xl font-bold dark:text-white'>
+    <div className="bg-white dark:bg-gray-800 sticky top-0 z-10">
+      <nav className="max-w-screen-lg mx-auto py-4 px-0 flex justify-between items-center sm:px-4">
+        <Link to="/" className="text-xl font-bold dark:text-white">
           devtom.me
         </Link>
-        <div className='flex items-center space-x-8'>
+        <div className="flex items-center space-x-8">
           <BsMoonStarsFill
             onClick={() => setDarkMode(!darkMode)}
-            className='cursor-pointer text-2xl dark:text-white'
+            className="cursor-pointer text-2xl dark:text-white"
           />
           <Link
-            to='/portfolio'
-            className='bg-gradient-to-r from-indigo-500 to-purple-500 text-white px-4 py-2 rounded-md'
+            to="/portfolio"
+            className="bg-gradient-to-r from-indigo-500 to-purple-500 text-white px-4 py-2 rounded-md"
           >
             Portfolio
           </Link>
